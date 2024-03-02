e8fc796a56cd9eb7e2b176addaa7254789f0a298
yubak <yubakei062318@outlook.jp>
2024-03-02 10:52:51 +0900

    軽微な変更

--- finished_product/parachute_avoidance.py
+++ finished_product/parachute_avoidance.py
@@ -28,7 +28,7 @@ def parachute():
     white_area=cv.countNonZero(img)
     black_area=all_area-white_area
     values=black_area/all_area*100
-    if values>=10:
+    if values>=10:#黒が%以上ならパラシュートアリと判断
         logger.info(f"{values}%")
         return -1
     else:
