c25de5b042d5dfec1b329471f1bb09d0a1fe4b42
yubak <yubakei062318@outlook.jp>
2024-03-02 10:34:40 +0900

    軽微な変更

--- finished_product/parachute_avoidance.py
+++ finished_product/parachute_avoidance.py
@@ -4,33 +4,33 @@ import math
 import numpy as np
 import picamera
 import logging.config
+import RPi.GPIO	as GPIO
 import time
-from PIL import Image
-
-#ログの設定の読み込み
-logging.config.fileConfig('logging.ini')
-logger = logging.getLogger(__name__)
-
+from PIL import Image 
 
 def parachute():
+    #ログの設定の読み込み
+    logging.config.fileConfig('logging.ini')
+    logger = logging.getLogger(__name__)
+    
     img=cv.imread(f"picture.jpg")
-    HEIGHT,WIDTH,_=img.shape
     img=cv.flip(img,-1)
-    img=img[HEIGHT//2:HEIGHT,0:WIDTH]#空の情報取得
+    HEIGHT,WIDTH,_=img.shape
+
     #二値化
     dst = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     cv.imwrite(f"gray.jpg",dst)
-    _,img_th=cv.threshold(dst,100,255,cv.THRESH_BINARY)
+    _,img_th=cv.threshold(dst,50,255,cv.THRESH_BINARY)
     cv.imwrite(f"binary.jpg",img_th)
     img=cv.imread(f"binary.jpg",cv.IMREAD_GRAYSCALE)
     #黒部分の画素数
-    all_area=HEIGHT//2*WIDTH
+    all_area=HEIGHT*WIDTH
     white_area=cv.countNonZero(img)
     black_area=all_area-white_area
-    if black_area>=0.2:
-        direction="back"
-        logging.info("Parachute avoidance behavior")
+    values=black_area/all_area*100
+    if values<=10:
+        logger.info(f"{values}%")
+        return -1
     else:
-        direction=0
-        logging.info("not recognizing the parachute")
-    return direction
+        logger.info(f"{values}%")
+        return 0
